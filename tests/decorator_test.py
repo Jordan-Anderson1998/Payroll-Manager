54cf5370129ebbe2044992ebb95d8c0f7dc5bf56
Jordan Anderson <jackedup534@gmail.com>
2026-01-19 14:49:58 -0600

    Add decorator to format string output to add symbol, add tests for decorator

--- tests/decorator_test.py
+++ tests/decorator_test.py
@@ -1,5 +1,16 @@
 import pytest
 from src.payroll_hub import *
+from tools.decorators.deco import output_formatter
+
+@output_formatter('$', True)
+def print_currency() -> str:
+
+    return '1000'
+
+@output_formatter('!', False)
+def print_statement() -> str:
+
+    return 'Hello There'
 
 def test_string_transformer_decorator():
     employee = Employee(name='Todd', age=21, email='something@spam.ca', postal_code='S0H 3G0')
@@ -8,3 +19,9 @@ def test_string_transformer_decorator():
     func_output = payroll_hub.add_employee(employee)
 
     assert expected_output == func_output
+
+
+def test_output_transformer_decorator():
+
+    assert print_currency() == '$1000'
+    assert print_statement() == 'Hello There!'
--- tools/decorators/deco.py
+++ tools/decorators/deco.py
@@ -13,3 +13,20 @@ def output_transformer(transformer: Any):
         return wrapper
 
     return decorator
+
+def output_formatter(symbol: str, before: bool):
+
+    def decorator(func: Callable) -> Callable:
+
+        def wrapper(*args, **kwargs):
+            nonlocal symbol
+
+            if before:
+                return f'{symbol}{func(*args, **kwargs)}'
+            else:
+                return f'{func(*args, **kwargs)}{symbol}'
+
+        return wrapper
+
+    return decorator
+
